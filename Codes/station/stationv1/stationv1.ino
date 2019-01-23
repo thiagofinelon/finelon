3581eadc1760eb5074d610deeeffe2b3a3c5391e
Thiago Finelon <thiago.finelon@outlook.com>
2019-01-22 23:05:34 -0300

    Atualização com correção da leitura dos sensores.

--- Codes/station/stationv1/stationv1.ino
+++ Codes/station/stationv1/stationv1.ino
@@ -17,23 +17,22 @@ RTC_DS1307 RTC;
 #define DUST_SENSOR_DIGITAL_PIN_PM25  6
 // Entradas Digitais
 #define DHTPIN                        8
-#define ANEMPIN                       3
-#define PLUVPIN                       4
+#define ANEMPIN                       2
+#define PLUVPIN                       9
 
 // Entradas Analógicas
 byte UVPIN = A2;
-byte VENTDIRPIN = A1;
+byte VENTDIRPIN = A0;
 byte PIRPIN = A3;
 
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-File file_test;
-int CS_SDcard = 10;
+const int CS_SDcard = 4;
 
 // Variables for RADIO
 const uint64_t addr = 0xF0F0F0F0E1LL;
-RF24 radio(23, 15);                       // (CE_Pin, CSN_Pin)
+//RF24 radio(23, 15);                       // (CE_Pin, CSN_Pin)
 char timestamp[15];
 
 
@@ -44,7 +43,7 @@ unsigned long endtime;
 
 // 
 byte Anem_counter;
-//
+
 // Constantes para o Pluviometro:
 const int Pluv_REED = 10;                              // The reed switch outputs to digital pin 10
 
@@ -53,18 +52,22 @@ int Pluv_val = 0;                                      // Current value of reed 
 int Pluv_old_val = 0;                                  // Old value of reed switch
 int Pluv_REEDCOUNT = 0;                                // This is the variable that hold the count of switching
 // Variables for TIME
-byte DAY;
-byte MONTH;
-byte YEAR;
-byte HOUR;
-byte MINUTE;
-byte SECOND;
+byte DAY = 22;
+byte MONTH = 01;
+int  YEAR  = 2019;
+byte HOUR = 15;
+byte MINUTE = 20;
+byte SECOND = 00;
 
 byte sd_flag;
 
-void setup() {
-  Serial.begin(115200);
+int counter = 0;
 
+void setup() {
+  
+  
+  Serial.begin(115200);
+  
   // Inicialização sensor de temperatura
   dht.begin();                                         // Initialize the DHT sensor
 
@@ -73,7 +76,7 @@ void setup() {
   // (http://www.ktudu.com/?p=21619)
   // (https://www.dobitaobyte.com.br/ler-e-escrever-no-sd-card-com-arduino/)
 
-  pinMode(CS_SDcard, OUTPUT);
+  
 
   // Inicialização RTC
   Wire.begin();
@@ -87,19 +90,35 @@ void setup() {
 
   DateTime now = RTC.now();
 
+  pinMode(UVPIN, INPUT);
+
+  // Inicialização Anemômetro
+  pinMode(ANEMPIN, INPUT);
+  digitalWrite(ANEMPIN, HIGH);
+
+  // Inicialização sensor de poeira
+  pinMode(DUST_SENSOR_DIGITAL_PIN_PM10, INPUT); // Pin_3 PM1.0
+  pinMode(DUST_SENSOR_DIGITAL_PIN_PM25, INPUT); // Pin_6 PM2.5
+
+  // Inicializacao Pluviometro
+  pinMode (PLUVPIN, INPUT_PULLUP);
+  digitalWrite(PLUVPIN ,HIGH);
+
+/*
   // Inicialização do Rádio
   radio.begin();
   radio.setChannel(28);
   radio.setPayloadSize(32);
   radio.setDataRate(RF24_1MBPS);
-
+*/
   // Wait 60 seconds for DSM501 to warm up
   for (int i = 1; i <= 15; i++)
   {
     delay(1000);                                            // 1s
   }
   starttime = millis();
-
+  //initialize_SD();
+  //Serial.println("Start reading");
 }
 
 void loop() {
@@ -109,19 +128,20 @@ void loop() {
   sprintf(timestamp, "%d%d%d%d%d%d*", YEAR, MONTH, DAY, HOUR, MINUTE, SECOND);
 /*
   // Imprime dados via serial
+  Serial.println("Teste");
   Serial.println(dht.readTemperature());
   Serial.println(dht.readHumidity());
   Serial.println(get_UV());
   Serial.println(get_SpeedWind());
   Serial.println(get_DirVento());
-  Serial.println(Pluv_REEDCOUNT);//*0.2794);
-  Pluv_REEDCOUNT = Pluv_REEDCOUNT * 0.25;
-  Serial.println(Pluv_REEDCOUNT);
+  Serial.println(get_PluvCalc());
   Serial.println(get_PM2());
   Serial.println(get_PM1());
   Serial.println(get_piranometerRead());
-*/
+  Serial.println("Finalizado");
+*/  
   //Imprime os dados no cartão microSD E Envia as variáveis via rádio
+  //send2SD("teste");
   send_data();
 
 }
@@ -211,7 +231,7 @@ void windvelocity() {
 float get_SpeedWind() {
   windvelocity();
   float Anem_RPM = ((Anem_counter) * 60) / (5000 / 1000); // Calculate revolutions per minute (RPM)
-  float speedwind = (((4 * 3.1415926535897932 * 147 * Anem_RPM) / 60) / 1000) * 3.6; // Calculate wind speed on km/h
+  float speedwind = (((4 * 3.141592 * 147 * Anem_RPM) / 60) / 1000) * 3.6; // Calculate wind speed on km/h
   return speedwind;
 }
 
@@ -225,42 +245,46 @@ void addcount() {
 
 
 int get_DirVento() {
-  int Dir_Vento_valor = analogRead(VENTDIRPIN);
-  int Dir_Vento_Winddir;
-  Dir_Vento_valor = map(Dir_Vento_valor, 0 , 1023 , 0 , 5);
+  float Dir_Vento_valor = analogRead(VENTDIRPIN);
+  Dir_Vento_valor = Dir_Vento_valor * (5.0 / 1023.0);
+
 
   if (Dir_Vento_valor <= 0.27) {
-   return Dir_Vento_Winddir = 315;
+   return 315;
   }
   else if (Dir_Vento_valor <= 0.32) {
-   return Dir_Vento_Winddir = 270;
+   return 270;
   }
   else if (Dir_Vento_valor <= 0.38) {
-   return Dir_Vento_Winddir = 225;
+   return 225;
   }
   else if (Dir_Vento_valor <= 0.45) {
-   return Dir_Vento_Winddir = 180;
+   return 180;
   }
   else if (Dir_Vento_valor <= 0.57) {
-   return Dir_Vento_Winddir = 135;
+   return 135;
   }
   else if (Dir_Vento_valor <= 0.75) {
-   return Dir_Vento_Winddir = 90;
+   return 90;
   }
   else if (Dir_Vento_valor <= 1.25) {
-   return Dir_Vento_Winddir = 45;
+   return 45;
   }
   else {
-   return Dir_Vento_Winddir = 000;
+   return 0;
   }
 }
 
 // =================================================
 // Funções do Pluviometro
 // =================================================
-void get_PluvCalc() {
+float get_PluvCalc() {
+  unsigned long timer = millis();
+  unsigned long millis();
+  long startTime = millis();
+  while (millis() < startTime + 5000) {
   // ler o estado do switch pelo pino de entrada:
-  int Pluv_val = digitalRead(Pluv_REED);                      //Read the status of the Reed swtich
+  int Pluv_val = digitalRead(PLUVPIN);                      //Read the status of the Reed swtich
 
   if ((Pluv_val == LOW) && (Pluv_old_val == HIGH)) {      //Check to see if the status has changed
     //delay(10);                                          // Delay put in to deal with any "bouncing" in the switch.
@@ -270,6 +294,8 @@ void get_PluvCalc() {
   else {
     Pluv_old_val = Pluv_val;                           //If the status hasn't changed then do nothing
   }
+  }
+  return Pluv_REEDCOUNT * 0.25;
 }
 
 // =================================================
@@ -339,23 +365,23 @@ void print_Date()
 
 void send2SD(char payload[])
 {
+  File file_test;  
   String fileName;
-
-  initialize_SD();
-
-  fileName = ("/" + String(YEAR) + "/" + String(MONTH) + "/" + String(DAY));
-
+ 
+  //fileName = ("/" + String(YEAR) + "/" + String(MONTH) + "/" + String(DAY));
+  /*
   char caminho[30];
   fileName.toCharArray(caminho, 30);
-
+  
   if (!SD.exists(caminho))
     SD.mkdir(caminho);
-
+  
   fileName =  fileName + "/" + HOUR + MINUTE + ".txt";
-
+ 
   fileName.toCharArray(caminho, 30);
-
-  file_test = SD.open(caminho, FILE_WRITE);
+ */ 
+  
+  file_test = SD.open("sujidade.txt", FILE_WRITE);
 
   file_test.write(payload);
 
@@ -364,8 +390,7 @@ void send2SD(char payload[])
   file_test.close();
 
   delay(100);
-
-  SD.end();
+  
 }
 
 void initialize_SD() {
@@ -389,24 +414,34 @@ void send_data()
   StaticJsonBuffer<512> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
 
+  float temp = dht.readTemperature();
+  float hum  = dht.readHumidity();
+  String  uv = get_UV();
+  float   ws   = get_SpeedWind();
+  int   wd   = get_DirVento();
+  float pluv = get_PluvCalc();
+  long int pm2  = get_PM2;
+  long int pm1  = get_PM1;
+  float igh  = get_piranometerRead();  
   
-
+  
   root["tipo"] = "sujidade";
-  root["timestamp"] = timestamp;
-  root["temp"] = dht.readTemperature();
-  root["hum"] = dht.readHumidity();
-  root["uv"] = get_UV();
-  root["ws_kmh"] = get_SpeedWind();
-  root["wind_dir"] = get_DirVento();
-  root["pluv"] = Pluv_REEDCOUNT;
-  root["pm25"] = get_PM2;
-  root["pm10"] = get_PM1;
-  root["igh"] = get_piranometerRead();
-
-  root.printTo(payload);
-
-  send2SD(payload);
-
+  root["timestamp"] = counter;
+  root["temp"] = temp;
+  root["hum"] = hum;
+  root["uv"] = uv;
+  root["ws_kmh"] = ws;
+  root["wind_dir"] = wd;
+  root["pluv"] = pluv;
+  root["pm25"] = pm2;
+  root["pm10"] = pm1;
+  root["igh"] = igh;
+  counter ++;
+  
+  root.prettyPrintTo(Serial);
+  //root.printTo(payload);
+  delay(300);
+  //send2SD(payload);
 
   //  while (establish_connection() != 1 || cont < 5) { // Tenta inicializar a conexao 5 vezes
   //    cont++;
@@ -419,11 +454,11 @@ void send_data()
   //}
 
   //cont = 0;
-
+/*
   while (send2rtu(payload) != 1 || cont < 5) { // Tenta enviar 5 vezes
     cont++;
   }
-
+*/
   //if (cont == 5)  {
   /* Se cont for igual a 5, nao conseguiu enviar, entao, deve-se armazenar a
     leitura no cartao sd e enviar na proxima vez e tambem retornar a funcao para
@@ -438,7 +473,7 @@ void send_data()
 
   /* Caso a conexao nao seja encerrada, a RTU tem um timer para encerrar sozinha */
 }
-
+/*
 boolean establish_connection() {
   char message[] = "#s";
   int i;
@@ -504,4 +539,4 @@ boolean ack_check(char id) {
     return 1;
   else
     return 0;
-}
+}*/
